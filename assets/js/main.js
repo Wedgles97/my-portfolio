535c193ef1d2d887d9e059f57238994cf25fde29
Wedgles97 <wedegles@gmail.com>
2023-08-29 18:19:38 -0300

    Refactor: Improved code structure and readability

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,28 @@
 /*=========== SHOW MENU ===========*/
-const toggleMenu = function () {
-  const btn = document.getElementById("nav-toggle"),
-    navToggle = document.querySelectorAll("nav .nav__toggle"),
-    links = document.querySelectorAll("nav ul li a"),
-    navMenu = document.querySelector(".nav__menu");
+const toggleMenu = () => {
+  const btn = document.getElementById("nav-toggle");
+  const navToggle = document.querySelectorAll("nav .nav__toggle");
+  const links = document.querySelectorAll("nav ul li a");
+  const navMenu = document.querySelector(".nav__menu");
 
   btn.addEventListener("click", function () {
-    const isActive = this.classList.contains("active");
-    const isNotActive = this.classList.contains("not-active");
-
-    if (isActive) {
+    if (this.classList.contains("active")) {
       this.classList.replace("active", "not-active");
-    } else if (isNotActive) {
+    } else if (this.classList.contains("not-active")) {
       this.classList.replace("not-active", "active");
     } else {
       this.classList.add("active");
     }
   });
 
-  for (const element of navToggle) {
-    element.addEventListener("click", function () {
+  navToggle.forEach((element) => {
+    element.addEventListener("click", () => {
       navMenu.classList.toggle("show-menu");
     });
-  }
+  });
 
-  for (const link of links) {
-    link.addEventListener("click", function () {
+  links.forEach((link) => {
+    link.addEventListener("click", () => {
       navToggle.forEach((element) => {
         element.classList.add("not-active");
         element.classList.remove("active");
@@ -33,53 +30,38 @@ const toggleMenu = function () {
 
       navMenu.classList.remove("show-menu");
     });
-  }
+  });
 };
 
 toggleMenu();
 
-/* When clicking on a menu item, hide the menu. */
-/*const links = document.querySelectorAll("nav ul li a");
-const navMenu = document.querySelector(".nav__menu");
-
-for (const link of links) {
-  link.addEventListener("click", function () {
-    navMenu.classList.remove("show-menu");
-  });
-}*/
-
 /*=========== ADD BLUR TO HEADER ===========*/
 const blurHeader = () => {
   const header = document.getElementById("header");
-  // When the scroll is greater than 50 viewport height, add the blur-header class to the a tag with the blur-header class
-  this.scrollY >= 50
-    ? header.classList.add("blur-header")
-    : header.classList.remove("blur-header");
+  const scrollThreshold = 50;
+  header.classList.toggle("blur-header", window.scrollY >= scrollThreshold);
 };
 window.addEventListener("scroll", blurHeader);
 
 /*=========== SHOW SCROLL UP ===========*/
 const scrollUp = () => {
   const scrollUp = document.getElementById("scroll-up");
-  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
-  this.scrollY >= 350
-    ? scrollUp.classList.add("show-scroll")
-    : scrollUp.classList.remove("show-scroll");
+  const scrollUpThreshold = 350;
+  scrollUp.classList.toggle("show-scroll", window.scrollY >= scrollUpThreshold);
 };
 window.addEventListener("scroll", scrollUp);
 
 /*=========== DARK LIGHT THEME ===========*/
 const themeButton = document.querySelector("#theme-button");
-
 themeButton.addEventListener("click", () => {
   document.documentElement.classList.toggle("dark-mode");
 });
 
+/* When clicking on any of the buttons in the navigation menu, the title of the button is visible */
 function smoothScrollToTarget(target) {
   const targetElement = document.querySelector(target);
   const navHeight = document.querySelector("nav").offsetHeight;
   const offset = 20;
-
   const targetPosition =
     targetElement.getBoundingClientRect().top +
     window.scrollY -
@@ -92,8 +74,7 @@ function smoothScrollToTarget(target) {
   });
 }
 
-// Aplicar rolagem suave usando delegação de eventos
-document.addEventListener("click", function (e) {
+document.addEventListener("click", (e) => {
   const link = e.target.closest(".nav__link, .home__scroll");
   if (link) {
     e.preventDefault();
